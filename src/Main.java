8ea2a4741bc7f494459cda855a3a8417d5469fe7
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-03-01 20:28:33 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,13 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
+        String[] test = {"aa", "dd", "tt", "aa", "aa", "tt", "pp", "dd", "kk", "pp"};
+        Set<String> set = new LinkedHashSet<>(Arrays.asList(test));
+        String[] result = set.toArray(new String[0]);
+        System.out.println(Arrays.toString(result));
 
+        test = Arrays.stream(test).distinct().toArray(String[]::new);
+        System.out.println(Arrays.toString(test));
 
     }
 }
